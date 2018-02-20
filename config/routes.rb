2b2cb8423f048a3475e0f51823897058ad7ab199
ayadij <ayadijkwel@yahoo.com>
2018-02-19 20:38:21 -0700

    customized paths for login and registration

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'list', to: 'pages#list'
 
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: "login", sign_out: "logout", sign_up: "register" }
   resources :logs
 
   root to: 'pages#home'
