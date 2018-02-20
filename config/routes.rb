19117c67e893762e4fa9233b98dd05a36faaa39f
ayadij <ayadijkwel@yahoo.com>
2018-02-19 19:16:34 -0700

    created pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/list'
+
+  get 'pages/log'
+
   devise_for :users
   resources :logs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
